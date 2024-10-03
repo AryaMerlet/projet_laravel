7863434f7a8c7580dba3d57373fcecf9d9b0d74e
AryaMerlet <arya.merlet@gmail.com>
2024-10-03 10:15:47 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Mail\infomail;
 
-Route::middleware(['auth', 'language'])->group(function () {
+Route::middleware(['auth','language'])->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -22,12 +23,10 @@ Route::middleware(['auth', 'language'])->group(function () {
     Route::get('motif/{motif}/restore', [MotifController::class, 'restore'])->withTrashed()->name('motif.restore');
 
     Route::resource('absence', AbsenceController::class);
-
+});
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->middleware(['auth', 'verified'])->name('dashboard');
-
-});
 
 
 require __DIR__.'/auth.php';
